9067841048fb53a7a221f3ac05fab9882e349390
shailesh131311 <bhadkeshailesh9@gmail.com>
2026-03-16 13:02:30 +0000

    container config

--- 01_basics/variable.js
+++ 01_basics/variable.js
@@ -1,6 +1,7 @@
-const accountId =22222
-let accountEmail="bhadkeshailes@google.com"
-var accountPass=777777
+const accountId = 22222
+let accountEmail = "bhadkeshailes@google.com"
+var accountPass = 777777
 
-console.table([accountId,accountEmail,accountPass]);
+// never use the var
+console.table([accountId, accountEmail, accountPass]);
 

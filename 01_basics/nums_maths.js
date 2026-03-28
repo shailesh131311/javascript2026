9d845531abdf15605e4e014f8063086acfc84b8d
shailesh131311 <bhadkeshailesh9@gmail.com>
2026-03-28 06:33:28 +0000

    javascript strings

---
+++ 01_basics/nums_maths.js
@@ -0,0 +1,53 @@
+
+// basic numbers 
+let a = 10;
+let b = 5.5;
+let c = -20;
+
+// console.log([a, b, c]);
+
+// special numbers 
+
+console.log(1/0);
+
+console.log("a"/2); // not a numer 
+
+
+// important number methods
+
+// toFixed () decimal control
+
+let num = 12.3456 
+console.log(num.toFixed(2));
+
+// parseInt and parseFloat ()
+
+console.log(parseInt("100px"));
+console.log(parseFloat("12.5"));
+
+// number ()
+
+console.log(Number("123"));
+
+
+// javascript maths 
+
+/// maths object 
+
+console.log(Math.PI);
+
+// common methods of the maths in the javascript
+
+// Math.round()
+
+console.log(Math.round(4.6));
+
+console.log(Math.round(4.2));
+
+// Math.floor
+
+console.log(Math.floor(4.9));
+
+// Math.ceil ()
+
+console.log(Math.ceil(4.1));

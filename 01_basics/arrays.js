ee9e1642e6c7969342bd3524d8c277be48e45c14
shailesh131311 <bhadkeshailesh9@gmail.com>
2026-03-28 08:13:15 +0000

    arrays in javascript

---
+++ 01_basics/arrays.js
@@ -0,0 +1,44 @@
+
+const Myarray = ["shailesh","pravin","prasann","soham","samadhan"];
+// console.log(Myarray);
+// console.log(Myarray[0]);   Access the index of an element 
+
+// common arrays methods in the javascript 
+// 1 push 
+
+// Myarray.push(0);
+
+// 2 Unshift
+// unshift adds the new element at the start of the array 
+
+// Myarray.unshift(0);
+
+// 3 pop
+
+// Myarray.pop()
+
+// length give the lenght of an array 
+
+// console.log(Myarray.length)
+
+// 🔥 Looping Arrays (Very Important)
+
+// 👉 1. for loop
+
+
+// for (let i = 1; i <= Myarray.length; i++) {
+//     console.log(Myarray);
+
+// }
+
+// 👉 2. forEach (best for beginners)
+
+// Myarray.forEach(function (item) {
+//     console.log(item);
+
+// });
+
+// 👉 3. map (very important 🔥)
+
+// const newarray = Myarray.map(item => item.toUpperCase());
+// console.log(newarray);
